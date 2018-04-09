28d84bfec0e5af3830def6a1804536301e392543
yeyifeng <yifeng.ye@suanzi.cn>
2018-04-09 14:06:30 +0800

    first commit

---
+++ swiftCode/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  swiftCode
+//
+//  Created by YeYiFeng on 2018/4/8.
+//  Copyright © 2018年 叶子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
